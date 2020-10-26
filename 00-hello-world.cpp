f71c7b8286101e334b687f832f3575cb85183093
Wojtek <s23050@pjwstk.edu.pl>
2020-10-26 15:27:55 +0100

    maslo

--- 00-hello-world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-auto main() -> int
-{
-    string imie = "Wojtek";
-    cout << "Hello " << imie;
-    return 0;
-}
---
+++ src/00-hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+auto main() -> int
+{
+    string imie = "Wojtek";
+    cout << "Hello " << imie;
+    return 0;
+}
